83ef5054b6723a1d210ff1646d1b1f3caf36a775
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2025-03-03 13:15:57 +0800

    1

---
+++ src/main/java/me/wjz/creeperhub/CreeperHubApplication.java
@@ -0,0 +1,13 @@
+package me.wjz.creeperhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreeperHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreeperHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/wjz/creeperhub/CreeperHubApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wjz.creeperhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreeperHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
